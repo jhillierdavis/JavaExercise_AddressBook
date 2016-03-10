e30ca086a29ae340769a7b340d1ceb57795a917e
Joe Hillier-Davis <joe.hillier-davis@tribalworldwide.co.uk>
2016-03-10 10:34:40 +0000

    Added interface for Address Book data.

---
+++ src/main/java/com/jhdit/addressbook/persistence/AddressBookData.java
@@ -0,0 +1,13 @@
+package com.jhdit.addressbook.persistence;
+
+import com.jhdit.addressbook.domain.Contact;
+
+import java.util.Set;
+
+/**
+ * Interface specifying contact for Address Book data (without specifying/providing persistence implementation)
+ */
+
+public interface AddressBookData {
+    Set<Contact> getContacts();
+}
