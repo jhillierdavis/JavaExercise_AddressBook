2afc5ae40231b24673716993bb83f9abeb5693b8
Joe Hillier-Davis <joe.hillier-davis@tribalworldwide.co.uk>
2016-03-10 07:16:00 +0000

    Initial implementation to process AddressBook content from CSV data.

---
+++ src/main/java/com/jhdit/addressbook/persistence/DataProcessor.java
@@ -0,0 +1,51 @@
+package com.jhdit.addressbook.persistence;
+
+import com.jhdit.addressbook.domain.Contact;
+import com.jhdit.addressbook.domain.Gender;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Loads Address Book data from a CSV file.
+ */
+
+public class DataProcessor {
+    private Set<Contact> set = new HashSet<>();
+
+    // Constants
+    private static final int NUMBER_OF_FIELDS = 3;
+    private static final String DELIMITOR = ",";
+
+    public DataProcessor(final File file) throws IOException {
+        List<String> lines = Files.readAllLines(file.toPath());
+        for (String line: lines)    {
+            processContactLine(line);
+        }
+    }
+
+    public Set<Contact> getContacts()  {
+        return this.set;
+    }
+
+    private void processContactLine(String line) {
+        String[] fields = line.split(DELIMITOR);
+
+        if (fields.length != NUMBER_OF_FIELDS) {
+            throw new IllegalArgumentException("Invalid contact line: " + line);
+        }
+
+        String fullname = fields[0].trim();
+        String genderValue = fields[1].trim().toUpperCase();
+        String dob = fields[2].trim();
+
+        Contact contact = new Contact(fullname, Gender.valueOf( genderValue ), dob);
+        set.add( contact );
+    }
+
+
+}
---
+++ src/test/java/com/jhdit/addressbook/persistence/DataProcessorTest.java
@@ -0,0 +1,60 @@
+package com.jhdit.addressbook.persistence;
+
+import com.jhdit.addressbook.domain.Contact;
+import org.junit.Test;
+import java.io.*;
+import java.net.URL;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class DataProcessorTest {
+
+    // Obtain test resource file
+    private static final String TEST_CSV_FILE = "/com/jhdit/addressbook/addressbook.csv";
+
+
+    @Test
+    public void canReadTestFileCorrectly() throws IOException {
+        // given:
+        InputStream is = getClass().getResourceAsStream(TEST_CSV_FILE);
+
+        // then:
+        assertNotNull(is);
+
+        // and: display for visual inspection
+        BufferedReader br = new BufferedReader(new InputStreamReader(is) );
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println("Line: `" + line + "'");
+        }
+    }
+
+    @Test
+    public void resourceFileIsAvailable() throws IOException {
+        URL url = this.getClass().getResource(TEST_CSV_FILE);
+        assertNotNull(url);
+
+        File resourceFile = new File(url.getFile());
+        assertTrue(resourceFile.exists());
+    }
+
+    @Test
+    public void testContactsProcessed() throws IOException  {
+        URL url = this.getClass().getResource(TEST_CSV_FILE);
+        assertNotNull(url);
+
+        File resourceFile = new File(url.getFile());
+        assertTrue(resourceFile.exists());
+
+        DataProcessor accessor = new DataProcessor( resourceFile );
+        Set<Contact> set = accessor.getContacts();
+
+        assertThat(set.size(), is(5));
+    }
+
+
+}
